bf98c3c28a41529291677b7313cae93223e4466e
KKAnumalasetty <kanumala@vols.utk.edu>
2020-04-05 22:33:09 -0500

    Sentiment analysis added

--- Twitter_App_Demo.py
+++ Twitter_App_Demo.py
@@ -9,25 +9,26 @@ from tweepy import API
 import os
 import json
 import streamlit as st
+import pandas as pd
+import re
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 class Twitter_client():
     def __init__(self,twitter_user=None):
         self.auth = Twitter_Authenticator().twitter_authenticator()
-        self.twitter_client = API(self.auth,wait_on_rate_limit=True)
+        self.api = API(self.auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
         self.twitter_user = twitter_user
         
         
     def get_user_tweets(self, num_tweets):
         tweets =[]
+        location = []
 #        print('user = ',self.twitter_client)
 #        print('timeline = ',self.twitter_client.user_timeline)
-        
-        # api = API(self.auth)
-        api = API(self.auth,wait_on_rate_limit=True)
-#        print('api  = ',api)
-       
-        for status in Cursor(api.user_timeline, screen_name=self.twitter_user, tweet_mode="extended").items(num_tweets):
+               
+        for status in Cursor(self.api.user_timeline, screen_name=self.twitter_user, tweet_mode="extended").items(num_tweets):
             tweets.append(status.full_text)
+            location.append(status.user.location)
         return tweets
 
 class Twitter_Authenticator():
@@ -38,12 +39,6 @@ class Twitter_Authenticator():
         access_token=os.environ.get('TWITTER_ACCESS_TOKEN')
         access_token_secret=os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
         
-#        print(api_key)
-#        print(api_secret_key)
-#        print(access_token)
-#        print(access_token_secret)
-        
-
         auth = OAuthHandler(api_key,api_secret_key)
         auth.set_access_token(access_token,access_token_secret)
         return auth
@@ -55,6 +50,7 @@ class Twitter_Streamer():
     def __init__(self):
         self.twitter_authenticator = Twitter_Authenticator()
         self.auth = Twitter_Authenticator().twitter_authenticator()
+        self.runanalyzer = Tweet_Sentiment_Analyzer()
 
     
     
@@ -68,10 +64,21 @@ class Twitter_Streamer():
 
     def stream_tweets_new(self,hash_tag_list,num_tweets):
         tweets =[]
-        api = API(self.auth,wait_on_rate_limit=True)
-        for status in Cursor(api.search, q=hash_tag_list).items(num_tweets):
+        locations = []
+        users = []
+        api = API(self.auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+        for status in Cursor(api.search, q=hash_tag_list+"&lang=en").items(num_tweets):
             tweets.append(status.text)
-        return tweets
+            locations.append(status.user.location)
+            users.append(status.user.screen_name)
+        tweets_DF = pd.DataFrame({
+                'user':users,
+                'tweet':tweets
+                })
+#        st.table(tweets_DF)
+        sentiment_tweets_DF =  self.runanalyzer.tweet_sentiment_analyzer_DF(tweets_DF)
+#        st.table(sentiment_tweets_DF)
+        return sentiment_tweets_DF
 
         
 
@@ -96,60 +103,101 @@ class StdOutListener(StreamListener):
            
     def on_error(self, status):
         if status==420:
-            print('Twitter rate limit breached and need to halt application')
+            st.write('Twitter rate limit breached and need to halt application')
             return False
-        print(status)
+        st.write(status)
         
         
         
-# class tweet_analytics():
-    
+class Tweet_Sentiment_Analyzer():
+    def __init__(self):
+        self.analyzer = SentimentIntensityAnalyzer()
         
+        
+    # Needs a dataframe with tweet column that has text in english language
+    def tweet_sentiment_analyzer_DF(self,dataframe):
+        tweet_DF = pd.DataFrame()
+        tweet_DF = dataframe.copy()
+        def clean_tweet(tweet):
+            if (len(tweet) >0):
+                tweet = re.sub("\n","",tweet)
+                tweet = re.sub(r'^https?:\/\/.*[\r\n]*', '', tweet, flags=re.MULTILINE)
+                tweet = re.sub(r'http\S+', '', tweet)
+                tweet = re.sub("&amp;","",tweet)
+                tweet = re.sub("@[\w]","",tweet)
+                tweet = re.sub("RT @[\w]","",tweet)
+            else:
+                st.write('Tweet has no characters to analyze')
+            return tweet
+        sentiment_scores = []
+        if (tweet_DF.empty == False) & (tweet_DF.shape[0]>0):
+            tweet_DF['my_clean_tweet'] = tweet_DF['tweet'].apply(lambda x: clean_tweet(x))
+            for index,each_row in enumerate(tweet_DF.values):
+                sentiment_score = self.analyzer.polarity_scores(tweet_DF.loc[index]['my_clean_tweet'])['compound']
+                sentiment_scores.append(sentiment_score)
+            tweet_DF.drop(['my_clean_tweet'], axis=1,inplace=True)
+            tweet_DF['Sentiment_Score'] = sentiment_scores
+            tweet_DF['Sentiment'] = tweet_DF['Sentiment_Score'].apply(lambda x: 'Positive' if x >0  else 'Negative')
+            tweet_DF= tweet_DF[['user','tweet','Sentiment','Sentiment_Score']]
+#            tweet_DF['Sentiment_Score'] = tweet_DF['Sentiment_Score'].apply(float)
+        else:
+            st.write('Sentiment analysis failed due to error, please check')
+        return tweet_DF
+
+
+    def tweet_sentiment_analyzer_list(self,tweet_list):
+        def clean_tweet(tweet):
+            tweet = re.sub("\n","",tweet)
+            tweet = re.sub(r'^https?:\/\/.*[\r\n]*', '', tweet, flags=re.MULTILINE)
+            tweet = re.sub("@[\w]","",tweet)
+            tweet = re.sub("RT @[\w]","",tweet)
+            return tweet
+        tweet_DF = pd.DataFrame()
+        sentiment_scores = []
+        for each_tweet in enumerate(tweet_list):
+            my_clean_tweet = clean_tweet(each_tweet)
+            sentiment_score = self.analyzer.polarity_scores(my_clean_tweet)
+            sentiment_scores.append(sentiment_score)
+        tweet_DF['tweet'] =  tweet_list
+        tweet_DF['Sentiment_Score'] = sentiment_scores
+        return tweet_DF
+
         
 if __name__ =="__main__":
     
     st.subheader(" Twitter real time data analytics and sentiment analysis by Karthik Anumalasetty ")
     
-#    github_img_link = "<a href='https://github.com/KKAnumalasetty/twitter_app_karthik' target='_blank' ><img src='https://user-images.githubusercontent.com/45413346/78455558-b6913c00-7664-11ea-90f4-744eb2acd6c1.jpg' width='125' height='100' style='float:left' /> </a>"
-#    linkedin_img_link = "<a href='https://www.linkedin.com/in/karthikanumalasetty/' target='_blank' ><img src='https://user-images.githubusercontent.com/45413346/78455807-fc9acf80-7665-11ea-810d-fee583f9e0c9.png' width='50' height='50' /> </a>"
-    
     combo_link = "<a href='https://www.linkedin.com/in/karthikanumalasetty/' target='_blank' ><img src='https://user-images.githubusercontent.com/45413346/78455807-fc9acf80-7665-11ea-810d-fee583f9e0c9.png' width='50' height='50' /> </a><a href='https://github.com/KKAnumalasetty/twitter_app_karthik' target='_blank' ><img src='https://user-images.githubusercontent.com/45413346/78455558-b6913c00-7664-11ea-90f4-744eb2acd6c1.jpg' width='125' height='100' /> </a>"
-
-#    ![](https://avatars3.githubusercontent.com/u/31112269?v=4&s=200)
-#    linkedin_url = "<div id='block_container'><div id='bloc1' style='float:left'><a href='https://www.linkedin.com/in/karthikanumalasetty/' target='_blank' > LinkedIn </a></div> <div style='float:left'> ||  </div>     <div id='bloc2'><a href='https://github.com/KKAnumalasetty/twitter_app_karthik' target='_blank' > Github </a></div> </div>"
-    
-#    st.write(linkedin_url,unsafe_allow_html=True)
-#    st.write(github_img_link,unsafe_allow_html=True)
-#    st.write(linkedin_img_link,unsafe_allow_html=True)
     st.write(combo_link,unsafe_allow_html=True)
     
     
     twitter_handle = "I'll search by Person/Twitter Handle (@realdonaldtrump)"
     twitter_hashtag = "I'll search by topic/hashtag (#corona virus)"
-    search_type = st.radio("Search Category", [twitter_handle,twitter_hashtag])
+    search_type = st.radio("Search Category", [twitter_hashtag,twitter_handle])
 
-#    st.write('user selected = ',search_type)
-    num_tweets = st.slider("How many Tweets you want to analyze", 1, 10,1)
-    
+    num_tweets = st.slider("How many Tweets you want to analyze", 1, 10,1,1)
     
     if search_type == twitter_handle:
         twitter_user = st.text_input('Enter Twitter Handle: ','@realdonaldtrump')
         twitter_client = Twitter_client(twitter_user)
         tweets = twitter_client.get_user_tweets(num_tweets)
         st.subheader("Live Tweets")
+#        st.table(tweets)
+#        runanalyzer = Tweet_Sentiment_Analyzer()
+#        sentiment_tweets_DF =  runanalyzer.tweet_sentiment_analyzer_list(tweets)
+#        st.table(sentiment_tweets_DF)
         for tweet in tweets:
             st.text(tweet)
     elif search_type == twitter_hashtag:
          fetched_tweets_file ='tweets.json'
          tweets = Twitter_Streamer()
          hash_tag_list = st.text_input('Type hashtag and hit enter: ','#COVID-19')
-#         btn = st.button("Run!")
-#         st.write('button = ',btn)
-         # Button will be inactive/False when clicked
          st.subheader("Live Tweets")
-         tweets =  tweets.stream_tweets_new(hash_tag_list,num_tweets)
-         for tweet in tweets:
-            st.text(tweet)
-
-    
-    
+         tweets_DF =  tweets.stream_tweets_new(hash_tag_list,num_tweets)
+         st.table(tweets_DF)
+#         def highlight_sentiment(df, column):
+#             is_max = pd.Series(data=False, index=df.index)
+#             is_max[column] = df[column] >= 0.2
+#             return ['background-color: yellow' if is_max.any() else 'background-color: red' for v in is_max]
+#         
+#         st.table(tweets_DF.style.apply(highlight_sentiment,column=['Sentiment_Score'],axis=0))
